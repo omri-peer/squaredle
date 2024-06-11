a205b5bd897129a0a62ffba963050a3e1fee2243
ogu33 <uri.peer7@gmail.com>
2024-06-11 19:21:50 +0300

    self explanatory

--- squaredle.py
+++ squaredle.py
@@ -62,16 +62,14 @@ def print_board(board):
     print(get_score(board))
     print(get_words(board))
 
-def get_stupid_board():
+def get_board():
     return [[get_letter() for i in range(4)] for j in range(4)]
-
 
 def get_score(board):
     return len(get_words(board))
 
-def nudge(board, scoring_func=get_score):
-    i = random.randint(0, 3)
-    j = random.randint(0, 3)
+def replace_letter(board, i, j, scoring_func):
+    curr_letter = board[i][j]
     max_score = 0
     best_letter = "A"
     for letter in ENGLISH_LETTERS:
@@ -80,13 +78,73 @@ def nudge(board, scoring_func=get_score):
         if score > max_score:
             max_score = score
             best_letter = letter
-    board[i][j] = best_letter
+    board[i][j] = curr_letter
+    return best_letter, max_score
+
+def nudge(board, scoring_func=get_score, steps=1):
+    for _ in range(steps):
+        i = random.randint(0, 3)
+        j = random.randint(0, 3)
+        best_letter, max_score = replace_letter(board, i, j, scoring_func)
+        board[i][j] = best_letter
+        return max_score
+
+def gradient_step(board, scoring_func=get_score):
+    max_score = 0
+    best_i = 0
+    best_j = 0
+    best_letter = "A"
+    for i in range(4):
+        for j in range(4):
+            letter, score = replace_letter(board, i, j, scoring_func)
+            if score > max_score:
+                best_i = i
+                best_j = j
+                best_letter = letter
+                max_score = score
+    board[best_i][best_j] = best_letter
+    return max_score
+
+def add_noise(board, squares=1):
+    for i in range(squares):
+        i = random.randint(0, 3)
+        j = random.randint(0, 3)
+        letter = random.choice(ENGLISH_LETTERS)
+        board[i][j] = letter
+
+def nudge_all_the_way(board, scoring_func=get_score):
+    prev_score = -1
+    score = scoring_func(board)
+    while True:
+        prev_score = score
+        score = nudge(board, scoring_func, steps=30)
+        if score > prev_score:
+            continue
+
+        gradient_step(board, scoring_func)
+        score = scoring_func(board)
+        if score == prev_score:
+            return
+
+
+def nudge_noise_grad(board, scoring_func=get_score):
+    pre_noise_score = -1
+    pre_nudge_score = -1
+    score = scoring_func(board)
+    while score > pre_noise_score:
+        while score > pre_nudge_score:
+            pre_nudge_score = score
+            score = nudge(board, scoring_func, steps=30)
+            print(score)
+        print(f'STUCK, {score=}')
+        pre_noise_score = score
+        add_noise(board, squares=5)
+        for i in range(5):
+            score = gradient_step(board, scoring_func)
+        print(f'MOVED, {score=}')
 
 words_tree = create_tree()
-print("Created tree")
-board = get_stupid_board()
+board = get_board()
 print_board(board)
-for _ in tqdm(range(10000)):
-    nudge(board)
-
+nudge_all_the_way(board, scoring_func=get_score)
 print_board(board)
