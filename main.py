4fd8d02baa6841ee41051f190d24726075780047
ogu33 <uri.peer7@gmail.com>
2024-06-14 12:04:17 +0300

    support letter counting in get_words

--- main.py
+++ main.py
@@ -17,8 +17,11 @@ def main():
     squaredle = Squaredle()
     board = load_board(BOARD)
     squaredle.print_board(board)
-    follow_favorites(board, scoring_func=squaredle.get_score)
-    squaredle.print_board(board)
+    words, usage = squaredle.get_words(board, True)
+    print('\n'.join([' '.join([str(x) for x in row]) for row in usage]))
+    print(sorted(words))
+    # follow_favorites(board, scoring_func=squaredle.get_score)
+    # squaredle.print_board(board)
 
 if __name__ == '__main__':
   main()
--- squaredle.py
+++ squaredle.py
@@ -28,36 +28,39 @@ class Squaredle:
     def __init__(self) -> None:
         self.words_tree = create_tree()
 
-    def words_with_prefix(self, board, p, last_dict, last, current):
-        words = []
+    def words_with_prefix(self, board, p, last_dict, last, current, words=[], usage=None):
         if " " in last_dict:
-            words.append(p)
+            if usage is not None:
+                if p not in words:
+                    for (i0, j0) in current:
+                        usage[i0][j0] += 1
+            words.add(p)
 
         for i in range(max(last[0] - 1, 0), min(last[0] + 2, 4)):
             for j in range(max(last[1] - 1, 0), min(last[1] + 2, 4)):
                 if ((i, j) not in current) and (board[i][j] in last_dict):
                     current.add((i, j))
-                    more_words = self.words_with_prefix(board, p + board[i][j], last_dict[board[i][j]], (i, j), current)
+                    self.words_with_prefix(board, p + board[i][j], last_dict[board[i][j]], (i, j), current, words, usage)
                     current.remove((i, j))
-                    words.extend(more_words)
 
-        return words
-
-    def get_words(self, board, verbose=False):
-        all_words = []
+    def get_words(self, board, with_usage=False, verbose=False):
+        all_words = set([])
         current = set([])
+        if with_usage:
+            all_usage = [[0 for _ in range(4)] for __ in range(4)]
+        else:
+            all_usage = None
         for i in range(4):
             for j in range(4):
                 current.add((i, j))
-                all_words.extend(self.words_with_prefix(board, board[i][j], self.words_tree[board[i][j]], [i,j], current))
+                self.words_with_prefix(board, board[i][j], self.words_tree[board[i][j]], [i,j], current, all_words, all_usage)
                 current.remove((i, j))
-        all_words = list(set(all_words))
 
         if verbose:
-            print(all_words)
+            print(sorted(list(all_words)))
             print(len(all_words))
 
-        return all_words
+        return all_words, all_usage
 
     def print_board(self, board):
         print("\n".join([" ".join(row) for row in board]))
@@ -65,4 +68,4 @@ class Squaredle:
         # print(self.get_words(board))
 
     def get_score(self, board):
-        return len(self.get_words(board))
+        return len(self.get_words(board)[0])
